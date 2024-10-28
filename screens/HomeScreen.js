2c58eac6217efada4dc093b6881e25c688b00767
cruz-Help-Link <itzdonnacruz00@gmail.com>
2024-10-28 06:12:59 -0500

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,31 +2,36 @@ import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import SearchBar from "../components/SearchBar";
 import WeatherCard from "../components/WeatherCard";
+import fetchWeather from "../utils/api";
 
 const HomeScreen = () => {
     const [weather, setWeather] = useState(null);
 
     const handleSearch = async (query) => {
         const data = await fetchWeather(query);
-        if(data) {
-            setWeather(data);
+        if (data) {
+            setWeather({
+                location: data.name,
+                temp: data.main.temp,
+                description: data.weather[0].description,
+            });
         }
     };
 
-    return(
+    return (
         <View style={styles.container}>
             <SearchBar onSearch={handleSearch} />
             {weather && <WeatherCard weather={weather} />}
         </View>
     );
+};
 
-}
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-
-    container:{
+    container: {
         flex: 1,
-        alignItems:'center',
-        justifyContent:'center',
+        alignItems: 'center',
+        justifyContent: 'center',
     }
-})
+});
